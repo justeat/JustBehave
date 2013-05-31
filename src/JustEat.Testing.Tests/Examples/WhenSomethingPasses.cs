6bcdc8f650651adcd67b498ca7f83458c36e097e
Peter Mounce <peter.mounce@just-eat.com>
2013-05-31 12:09:04 +0100

    Make XUnit version of BehaviourTest

---
+++ src/JustEat.Testing.Tests/Examples/WhenSomethingPasses.cs
@@ -0,0 +1,34 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace JustEat.Testing.Tests.Examples
+{
+    public class WhenSomethingPasses : XBehaviourTest<HappyThing>
+    {
+        private string _result;
+
+        protected override void Given()
+        {
+            _result = "foo";
+        }
+
+        protected override void When()
+        {
+            _result = SUT.ToString();
+        }
+
+        [Fact]
+        public void ShouldHaveString()
+        {
+            _result.ShouldNotBe(null);
+        }
+
+        [Fact]
+        public void ShouldHaveChangedFromInitial()
+        {
+            _result.ShouldNotBe("foo");
+        }
+    }
+
+    public class HappyThing {}
+}
---
+++ src/JustEat.Testing/XBehaviourTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace JustEat.Testing
+{
+    public abstract class XBehaviourTest<TSystemUnderTest> : IDisposable
+    {
+        private ExceptionMode _exceptionMode = ExceptionMode.Throw;
+        private MockManager _mockManager;
+        private AutoMocker<TSystemUnderTest> _autoMocker;
+        protected Logger Log { get; set; }
+
+        protected XBehaviourTest()
+        {
+#if DEBUG
+            var level = LogLevel.Trace;
+#else
+			var level = LogLevel.Warn;
+#endif
+            SimpleConfigurator.ConfigureForTargetLogging(new ColoredConsoleTarget {Layout = "${message}"}, level);
+            Log = LogManager.GetCurrentClassLogger();
+
+            Setup();
+        }
+
+        protected Exception ThrownException { get; private set; }
+
+        protected TSystemUnderTest SUT { get; private set; }
+
+        [DebuggerStepThrough]
+        private void Setup()
+        {
+            _mockManager = new MockManager();
+            _autoMocker = new AutoMocker<TSystemUnderTest>(_mockManager);
+
+            Given();
+
+            SUT = CreateSystemUnderTest();
+
+            try
+            {
+                When();
+            }
+            catch (Exception ex)
+            {
+                if (_exceptionMode == ExceptionMode.Record)
+                {
+                    ThrownException = ex;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            finally
+            {
+                Teardown();
+            }
+        }
+
+        public virtual void PostAssertTeardown() {}
+
+        protected abstract void Given();
+
+        [SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", Justification = "When really is the best name for this message")]
+        protected abstract void When();
+
+        protected virtual void Teardown() {}
+
+        /// <summary>
+        /// Override this if TSystemUnderTest doesn't have a parameterless constructor.
+        /// </summary>
+        protected virtual TSystemUnderTest CreateSystemUnderTest()
+        {
+            return _autoMocker.ConstructedObject();
+            //return Activator.CreateInstance<TSystemUnderTest>();
+        }
+
+        protected void RecordAnyExceptionsThrown()
+        {
+            _exceptionMode = ExceptionMode.Record;
+        }
+
+        protected TMock Mock<TMock>() where TMock : class
+        {
+            return _mockManager.Mock<TMock>();
+        }
+
+        #region Nested type: ExceptionMode
+
+        private enum ExceptionMode
+        {
+            Throw,
+            Record
+        }
+
+        #endregion    }
+
+        public void Dispose()
+        {
+            PostAssertTeardown();
+        }
+    }
+}
