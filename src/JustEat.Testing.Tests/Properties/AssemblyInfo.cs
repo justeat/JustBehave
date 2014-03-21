068086467dbbc32aacd3703f7b4f3b59846a82a4
Anton <anton.jefcoate@just-eat.com>
2014-03-21 13:00:04 +0000

    Adding assembly info files in prep for 0 ruby

---
+++ src/JustEat.Testing.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("JustEat.Testing.Tests")]
+[assembly: AssemblyCompany("Just-Eat Holding Ltd")]
+[assembly: AssemblyCopyright("Copyright 2014, by Just-Eat Holding Ltd")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0")]
+
+[assembly: CLSCompliant(false)]
