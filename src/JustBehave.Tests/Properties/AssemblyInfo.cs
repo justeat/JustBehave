1aa71ab46f403b02b468db53a1f36ba7e00a5ee6
David Whitney <david.whitney@just-eat.com>
2014-04-02 16:01:02 +0100

    Added missing assemblyinfo / config files.

---
+++ src/JustBehave.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("JustBehave.Tests")]
+[assembly: AssemblyCompany("Just Eat")]
+[assembly: AssemblyCopyright("Copyright 2011-2014 Just Eat, Inc. or its affiliates. All Rights Reserved.")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0")]
+
+[assembly: CLSCompliant(false)]
